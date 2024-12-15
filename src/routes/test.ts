7a79c4b8d71c57bea3cc9dd65f17c3c7c9a9826b
franss <franss40@gmail.com>
2024-12-15 12:43:05 +0100

    refactor cambio en la forma de recuperar promesas de datos

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -3,25 +3,31 @@ import getAsks from '@src/services/ask'
 import express from 'express'
 const routerTest = express.Router()
 
-// Recuperar todos los tests (GET api/tests)
-routerTest.get('/tests', async (_req, res) => {
-  try {
-    const tests = await getTests()
-    res.json(tests)
-  } catch (error) {
-    res.status(500).send({ message: `Se ha producido un error. ${error}` })
-  }
+// Recuperar todos los tests junto con el nombre de usuario (GET api/tests)
+routerTest.get('/tests', (_req, res) => {
+  getTests()
+    .then(tests => {
+      res.json(tests)
+    })
+    .catch((error) => {
+      res.status(500).send({ message: `Se ha producido un error. ${error}` })
+    })
 })
 
-// Recuperar todas las preguntas de un determinado test (GET api/test/:idTest)
-routerTest.get('/test/:idTest', async (_req, res) => {
-  try {
-    const idTest = Number(_req.params.idTest)
-    if (isNaN(idTest)) throw 'El parámetro debe ser un número'
-    const asks = await getAsks(idTest)
-    res.json(asks)
-  } catch (error) {
-    res.status(500).send({ message: `Se ha producido un error. ${error}` })
+// Recuperar todas las preguntas de un determinado test junto nombre de usuario
+// y el nombre del test (GET api/test/:idTest)
+routerTest.get('/test/:idTest', (_req, res) => {
+  const idTest = Number(_req.params.idTest)
+  if (!isNaN(idTest)) {
+    getAsks(idTest)
+      .then(asks => {
+        res.json(asks)
+      })
+      .catch((error) => {
+        res.status(500).send({ message: `Se ha producido un error. ${error}` })
+      })
+  } else {
+    res.status(500).send({ message: 'El parámetro debe ser un número' })
   }
 })
 
